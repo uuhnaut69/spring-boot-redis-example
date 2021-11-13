06f16472bf014489384597a6dc7d4aa0a8a6466c
uuhnaut69 <uuhnaut96@gmail.com>
2021-11-13 12:31:33 +0700

    Init project

---
+++ src/main/java/com/uuhnaut69/example/SpringBootRedisExampleApplication.java
@@ -0,0 +1,12 @@
+package com.uuhnaut69.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRedisExampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootRedisExampleApplication.class, args);
+  }
+}
